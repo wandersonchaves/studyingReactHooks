5d1d4912c97b3b25dcc827ac46c9a80eaa337795
Wanderson Chaves <wandersonscpibr@gmail.com>
2021-08-07 23:02:05 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: "repo-1" },
-    { id: 2, name: "repo-2" },
-    { id: 3, name: "repo-3" },
-  ]);
+  const [repositories, setRepositories] = useState([]);
 
-  function handleAddRepositories() {
-    setRepositories([
-      ...repositories,
-      { id: Math.random(), name: "Novo repo" },
-    ]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    const res = await fetch(
+      "https://api.github.com/users/wandersonchaves/repos"
+    );
+    const data = await res.json();
+
+    setRepositories(data);
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter((repo) => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
   }
 
   return (
     <>
       <ul>
         {repositories.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name} {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         ))}
       </ul>
-      <button onClick={handleAddRepositories}>Adicionar repositório</button>
     </>
   );
 }
